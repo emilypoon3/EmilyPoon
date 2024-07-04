f127cf68cb0fc5868f31b3fef1af7b2ee6d25e85
emilypoon3 <emilypoon@g.ucla.edu>
2024-07-03 18:01:05 -0700

    Add files via upload

---
+++ aboutme.php
@@ -0,0 +1,43 @@
+#!/usr/bin/env php
+<!DOCTYPE html>
+<html lang="en">
+
+  <head>
+    <title>About</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="style.css?v=<?php echo rand(); ?>">
+  </head>
+
+  <body>
+  <nav class="navbar">
+      <ul>
+        <li><a href="https://drive.google.com/file/d/1LaSj0SLofoXjD3YkWe0g2jK5QnZStxlK/view?usp=sharing">Resume</a></li>
+        <li><a href="aboutme.php">About Me</a></li>
+        <li><a href="index.php">My Work</a></li>
+      </ul>
+    </nav>
+    <div class="header">
+      <h1 style="margin-bottom:10px; margin-top: 10px;">Emily Poon</h1>
+      <p style="font-size:18px; margin-left:5px;">Aspiring UX/UI Designer.</p>
+    </div>
+      <div class="div">
+        <br>
+        <figure><img class="headshot" src="IMG_3725.jpg"  alt="Failed to load."> </figure>
+        <div class="righttext">
+          <h1 style="font-size: 40px;">Hello!</h1> 
+          <h3>My name is Emily Poon.</h3>
+          <p style="line-height: 1.8;">I'm a second year Cognitive Science major at UCLA with experience in design and illustration, looking to break into UX/UI. </p>
+          <p style="line-height: 1.8;">I aim to create designs that are both meaningful and accessible, in a way that allows them to achieve tangible impacts while empowering real communities. As of right now, I am looking for more opportunities to further my skills and diversify my projects.  </p>
+          <h4>emilypoon@g.ucla.edu | (714) 916-1123 |  <a class="link" href="www.linkedin.com/in/emilymaepoon">Linkedin</a></h4>
+        </div>
+        <br>
+      </div>
+
+    <div class="footer">
+      <h4>Coded by me!</h4>
+    </div>
+
+  </body>
+
+</html>
